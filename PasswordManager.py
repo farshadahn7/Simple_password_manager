37ae4356e2d6d0f187ea4436dee6631dda274946
farshad <farshadan7@gmail.com>
2025-08-23 19:03:48 +0330

    execute sql method added to passwordmanager class. It hindered our code from redundancy

--- PasswordManager.py
+++ PasswordManager.py
@@ -23,9 +23,7 @@ class PasswordManager:
                 self.conn.rollback()
             sql = "insert into passwords (site_id,username,password_hash) values (%s,%s,%s)"
             data = (site_id, username, password)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql,data=data,commit=True)
 
             print("Data insertion completed successfully.")
         except psycopg2.errors as e:
@@ -35,9 +33,7 @@ class PasswordManager:
         try:
             sql = "INSERT INTO site (site_name) VALUES (%s)"
             data = (site_name,)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql,data=data,commit=True)
 
         except psycopg2.errors as e:
             print(f"Opps something went wrong during the insertion, errors{e}")
@@ -46,9 +42,7 @@ class PasswordManager:
         try:
             sql = "update site set site_name=%s where site_name=%s"
             data = (new_site_name, site_name)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql,data=data,commit=True)
         except psycopg2.errors as e:
             print(f"Oops something went wrong. errors {e}")
 
@@ -59,9 +53,7 @@ class PasswordManager:
             site_id = self.get_site_id(site_name)[0][0]
             sql = "update passwords set password_hash = %s where site_id = %s and username = %s"
             data = (password, site_id, username)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql,data=data,commit=True)
             print("Password updated_successfully")
         except IndexError:
             print(f"{site_name} does not exists.")
@@ -72,9 +64,7 @@ class PasswordManager:
             site_id = self.get_site_id(site_name)[0][0]
             sql = "update passwords set username = %s where site_id = %s and username = %s"
             data = (new_username, site_id, username)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql,data=data,commit=True)
             print("username updated successfully.")
         except IndexError:
             print(f"{site_name} does not exists.")
@@ -84,11 +74,9 @@ class PasswordManager:
 
     def get_site_id(self, site_name):
         try:
-            cursor = self.get_cursor()
             sql = "select id from site where site_name=%s"
             data = (site_name,)
-            cursor.execute(sql, data)
-            query = cursor.fetchall()
+            query = self.execute_sql(sql=sql,data=data,fetchall=True)
             return query
         except psycopg2.errors as e:
             print(f"opps sth went wrong by returning site id. Errors: {e}")
@@ -98,9 +86,7 @@ class PasswordManager:
             site_id = self.get_site_id(site_name)[0][0]
             sql = "select site_name, username, password_hash from passwords inner join site on site_id = site.id where site_id = %s"
             data = (site_id,)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            query = cursor.fetchall()
+            query = self.execute_sql(sql=sql,data=data,fetchall=True)
             return query
         except IndexError:
             print(f"{site_name} does not exists.")
@@ -111,9 +97,7 @@ class PasswordManager:
     def get_all(self):
         try:
             sql = "select site_name, username, password_hash from passwords inner join site on site_id = site.id"
-            cursor = self.get_cursor()
-            cursor.execute(sql)
-            query = cursor.fetchall()
+            query = self.execute_sql(sql=sql, fetchall=True)
             return query
         except psycopg2.errors as e:
             print(f"Oops something went wrong. Errors: {e}")
@@ -123,9 +107,7 @@ class PasswordManager:
             site_id = self.get_site_id(site_name)[0][0]
             sql = "delete from passwords where site_id = %s and username = %s"
             data = (site_id, username)
-            cursor = self.get_cursor()
-            cursor.execute(sql, data)
-            self.conn.commit()
+            self.execute_sql(sql=sql, data=data, commit=True)
             print("Deleted successfully")
         except IndexError:
             print(f"{site_name} does not exists.")
@@ -135,3 +117,15 @@ class PasswordManager:
 
     def close(self):
         self.conn.close()
+
+    def execute_sql(self, sql, data=None, commit=None, fetchall=None):
+        cursor = self.get_cursor()
+        try:
+            cursor.execute(sql, data)
+            if commit:
+                self.conn.commit()
+            if fetchall:
+                query = cursor.fetchall()
+                return query
+        except psycopg2.errors as e:
+            print(f"Oops something went wrong. Errors: {e}")
